4ab6b75fead2f9d8bbd069a202efb573e463cd60
hana <asufanah@gmail.com>
2016-04-01 21:38:50 +0900

    update sample project

--- sample/spring-boot-ddd-sample/src/main/java/com/github/asufana/domain/models/threads/repo/ThreadRepo.java
+++ sample/spring-boot-ddd-sample/src/main/java/com/github/asufana/domain/models/threads/repo/ThreadRepo.java
@@ -1,10 +1,9 @@
 package com.github.asufana.domain.models.threads.repo;
 
-import org.springframework.stereotype.Repository;
-
 import com.github.asufana.ddd.repo.AbstractRepository;
 import com.github.asufana.domain.models.threads.Thread;
 import com.github.asufana.domain.models.threads.vo.ThreadTitle;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ThreadRepo extends AbstractRepository<Thread> {
